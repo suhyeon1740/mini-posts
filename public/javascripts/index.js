301a06c258165a6914063993dac7931c62bb4db1
elly <suhyeon1740@gmail.com>
2020-10-07 18:27:47 +0900

    코드 줄정리

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,15 +3,14 @@ $like.forEach((like) => {
     like.onclick = ({ target }) => {
         const id = target.parentNode.dataset.id
         const type = target.classList.contains("filled") ? "decrease" : "increase"
-        fetch(`http://localhost:3000/like?id=${id}&type=${type}`)
-            .then(() => {
-                if(type=="increase") {                    
-                    target.classList.add('fas','filled')
-                    target.classList.remove('far')
-                } else {                    
-                    target.classList.remove('fas', 'filled')
-                    target.classList.add('far')
-                }
-            })
+        fetch(`http://localhost:3000/like?id=${id}&type=${type}`).then(() => {
+            if (type == "increase") {
+                target.classList.add("fas", "filled")
+                target.classList.remove("far")
+            } else {
+                target.classList.remove("fas", "filled")
+                target.classList.add("far")
+            }
+        })
     }
 })
